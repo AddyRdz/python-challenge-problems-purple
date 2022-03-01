915ffcb06c90d9f357f5c0938e1f57a8f2a5ed2b
Addy Rodriguez <rdz.33.a@gmail.com>
2022-02-28 20:38:13 -0600

    warm up

--- day1/day1-solutions.py
+++ day1/day1-solutions.py
@@ -1,7 +1,14 @@
 # Problems Completed [ ]
 
 # Your test/solution code here 
-
+def even_range(start,end):
+    even_range=[]
+    if (start%2!=0):
+        start=start+1
+    for num in range(start,end,2):
+        even_range.append(num)
+    return even_range
+print(even_range(4,11))
 # Problem 0
 
 # Problem 1
