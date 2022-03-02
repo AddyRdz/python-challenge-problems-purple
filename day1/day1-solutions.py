8de587117c3975aa10d0fd8861aa8a4b1294280c
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-01 22:38:59 -0600

    Problem 0

--- day1/day1-solutions.py
+++ day1/day1-solutions.py
@@ -2,6 +2,7 @@
 
 # Your test/solution code here 
 from cmath import nan
+from operator import truediv
 
 
 def even_range(start,end):
@@ -14,7 +15,19 @@ def even_range(start,end):
 print(even_range(4,11))
 
 # Problem 0
-
+def phrase_finder(words,phrase):
+    phrase_split = phrase.split()
+    print(phrase_split)
+    counter = 0
+    for w in words:
+        for p in phrase_split:
+            if w ==p:
+                counter +=1
+    if counter ==2:
+        return True
+    else:
+        return False
+print(phrase_finder(['world', 'bootcamp', 'hello', 'prep'], 'hello world'))
 # Problem 1
 def hammingDistance(string1,string2):
     c = 0
--- day2/day2-solutions.py
+++ day2/day2-solutions.py
@@ -2,6 +2,8 @@
 
 # Your test/solution code here 
 
+def reverse_string(string):
+    for w in string
 # Problem 0
 
 # Problem 1
