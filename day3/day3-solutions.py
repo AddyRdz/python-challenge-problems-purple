c57a429fb098077d48d73ba7d7edbb7311185628
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-05 23:08:41 -0600

    updates

--- day3/day3-solutions.py
+++ day3/day3-solutions.py
@@ -1,11 +1,25 @@
 # Problems Completed [ ]
 
 # Your test/solution code here 
+def intersect(list1, list2):
+    inter = []
+    for ele1 in list1:
+        for ele2 in list2:
+            if ele1 == ele2:
+                inter.append(ele1)
+    return inter
+print(intersect(['a', 'b', 'c'], ['x', 'y', 'z']))
 
 # Problem 0
-
+# Write a function `min_max_product(list)` that return the product between the
+# Use min() and max() to find the largest and smallest number of the list.
+# largest value and the smallest value in the list.
+# sum() both the product of min() and max().
+# def min_max_product(list):
 # Problem 1
-
+# def find_highest_priced():
+# use max() to find highest
+# key=priced.get returns highest value
 # Problem 2
 def bracket_matcher(input):
   stack = []
@@ -34,5 +48,6 @@ def bracket_matcher(input):
 
 
 # Problem 3
+# def count_the_bits():
 
 
--- day4/day4-solutions.py
+++ day4/day4-solutions.py
@@ -1,9 +1,13 @@
 # Problems Completed [ ]
-
+# def fuzz_bizz(max):
+# find max, return numbers that do not go over.
+# % by 2 or 7, if not do not print.
 # Your test/solution code here 
 
 # Problem 0
-
+# def multiples(max,num):
+# mutiples of sum
+# if max < num print multiples
 # Problem 1
 
 # Problem 2
