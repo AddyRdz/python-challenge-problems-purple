88b271bc44f75b90154a5f9b96cae049ec4b5c4d
Addy Rodriguez <rdz.33.a@gmail.com>
2022-03-06 19:27:20 -0600

    Updates to solutions

--- day4/day4-solutions.py
+++ day4/day4-solutions.py
@@ -11,14 +11,21 @@
 
 # Problem 1
 # def flatten():
-# Accepts nested lists and returns one long list.
+# Accepts nested lists and returns one long list
 # recursion?
 # tuples also included
 
 # Problem 2
 # def is_winning_ticket():
+# first value a nested list
+# second value a num
+# characters character code match num = true
 
 # Problem 3
 # def grid_trip():
+# value depending on positions
+# first num is x position
+# second num is y position
+# direction characters are UPPER CASE
 
 
--- day5/day5-solutions.py
+++ day5/day5-solutions.py
@@ -2,17 +2,35 @@
 
 # Your test/solution code here 
 # def list_range(min, max, step):
+# 3 numbers as params
+# list will be from min-max
 
 # Problem 0
 # def value_replace(list, dict):
+# in list and dictionary
+# return new list
 
 # Problem 1
 # def add_checker():
+# 2 arguments
+# first list at least 2 int.
+# list sorted in ascending order
+# list by:
+# add checker return false if no two integers
+#  are equal to second argument
 
 # Problem 2
 # def to_camel_case():
+# single string as argument
+# return as camel cased
+# replace method?
+
 
 # Problem 3
 # def total_task_time():
+# two arguments
+# first list = queue
+# second int rep threads
+# return an int that is total time
 
 
